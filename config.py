e80e449cc2154b7a3d8f07d1c5f890e815884732
Ramir Vorobyev <rrramirrr@yandex.ru>
2024-02-05 23:18:50 +0300

    first ui

---
+++ config.py
@@ -0,0 +1,22 @@
+TITLE_WINDOW = "Игра жизнь"
+
+FPS = 60
+
+TITLE_MENU = 'assets/Заголовок.png'
+
+BUTTONS_OFF = {
+    "Создать игру": 'assets/Создать игру_off.png',
+    "Статистика": 'assets/Статистика_off.png',
+    "Выйти": 'assets/Выйти_off.png',
+}
+
+BUTTONS_ON = {
+    "Создать игру": 'assets/Создать игру.png',
+    "Статистика": 'assets/Статистика.png',
+    "Выйти": 'assets/Выйти.png',
+}
+
+COLOUR = {
+    "BLACK": (0, 0, 0),
+}
+
--- main.py
+++ main.py
@@ -1,8 +1,60 @@
 import pygame
-from screeninfo import get_monitors
+from config import *
 
-m = get_monitors()
+DISPLAY = pygame.display.set_mode((900, 500))
+pygame.display.set_caption(TITLE_WINDOW)
 
 class Game():
-    pygame.init()
-    SCREEN = pygame.display.set_mode((1280,720), pygame.FULLSCREEN)
+
+    def start(self):
+        pygame.init()
+        clock = pygame.time.Clock()
+
+        RUN = True   
+        while RUN:
+            clock.tick(FPS)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    RUN = False
+
+            DrawUI.main_menu()
+            pygame.display.update()
+
+        pygame.quit()
+
+class Element_UI():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self):
+        DISPLAY.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class DrawUI():
+    
+    def main_menu():
+        DISPLAY.fill(COLOUR["BLACK"])
+
+        title = pygame.image.load(TITLE_MENU).convert_alpha()
+        new_game_off = pygame.image.load(BUTTONS_OFF["Создать игру"]).convert_alpha()
+        stats_off = pygame.image.load(BUTTONS_OFF["Статистика"]).convert_alpha()
+        quit_off = pygame.image.load(BUTTONS_OFF["Выйти"]).convert_alpha()
+
+        title_menu = Element_UI(34, 45, title).draw()
+        new_game_off_button = Element_UI(34, 180, new_game_off).draw()
+        stats_off_button = Element_UI(34, 270, stats_off).draw()
+        quit_off_button = Element_UI(34, 360, quit_off).draw()
+        
+        pygame.display.update()
+
+        
+
+game = Game()
+game.start()
+pygame.quit()
+
+
+
